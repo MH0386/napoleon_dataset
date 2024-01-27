366bbccc592aade277f5fb16292a87ce44365e71
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-01-27 20:53:37 +0200

    d

--- f.py
+++ f.py
@@ -1,26 +1,22 @@
-f = open('data.txt', 'r', encoding='utf-8')
-content = f.read()
-new_content = content[:]
+with open('data2.txt', 'r', encoding='utf-8') as f:
+    content = f.read()
+    new_content = content[:]
 
-for i in range(len(content)):
-    if new_content[i] == ' ' and new_content[i - 1] == '.':
-        new_content = new_content[:i] + '.' + new_content[i + 1:]
+    for i in range(len(content)):
+        if new_content[i] == ' ' and new_content[i - 1] == '.':
+            new_content = f'{new_content[:i]}.{new_content[i + 1:]}'
 
-for i in range(len(content)):
-    if new_content[i] == '.':
-        new_content = new_content[:i] + '\n' + new_content[i + 1:]
+    for i in range(len(content)):
+        if new_content[i] == '.':
+            new_content = new_content[:i] + '\n' + new_content[i + 1:]
 
-for i in content:
-    if i == '"' or i == "'":
-        new_content = new_content.replace(i, ' ')
-
-f.close()
+    for i in content:
+        if i in ['"', "'"]:
+            new_content = new_content.replace(i, ' ')
 
 new_content = new_content.split('\n')
 new_content = list(filter(lambda x: x != '', new_content))
-ff = open('Napoleon.csv', 'w', newline='')
-print(new_content)
-for i in new_content:
-    ff.write('"'+i+'",\n')
-
-ff.close()
+with open('Napoleon2.csv', 'w', newline='') as ff:
+    print(new_content)
+    for i in new_content:
+        ff.write(f'"{i}' + '"\n')
--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 # Make a request to the website
-r = requests.get('https://www.history.com/topics/european-history/napoleon')
+r = requests.get('https://www.rmg.co.uk/stories/topics/napoleon-bonaparte#:~:text=Napoleon%20Bonaparte%20was%20one%20of,at%20military%20school%20in%20France.')
 
 # Parse the HTML content
 soup = BeautifulSoup(r.text, 'html.parser')
 
 # get the text of the html page
 print(soup.get_text(separator='\n'))
+
+
+'''
+https://www.history.com/topics/european-history/napoleon
+'''
