293b35cd2424ae3dac38b5cd8f6547b57c7c22c7
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-01-28 00:27:42 +0200

    d

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # Make a request to the website
-r = requests.get('https://www.rmg.co.uk/stories/topics/napoleon-bonaparte#:~:text=Napoleon%20Bonaparte%20was%20one%20of,at%20military%20school%20in%20France.')
+r = requests.get('')
 
 # Parse the HTML content
 soup = BeautifulSoup(r.text, 'html.parser')
@@ -13,4 +13,6 @@ print(soup.get_text(separator='\n'))
 
 '''
 https://www.history.com/topics/european-history/napoleon
+https://www.history.com/news/napoleon-exile-death
+https://www.rmg.co.uk/stories/topics/napoleon-bonaparte#:~:text=Napoleon%20Bonaparte%20was%20one%20of,at%20military%20school%20in%20France.
 '''
