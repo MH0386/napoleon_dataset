a556344cb0742c3549d98a22280cb28b869fac81
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-01-28 02:08:48 +0200

    d

--- t2csv.py
+++ t2csv.py
@@ -2,7 +2,6 @@ f = open('data.txt', 'r', encoding='utf-8')
 content = f.read()
 content = content.split('\n')
 content = list(filter(lambda x: x != '', content))
-with open('napoleon.csv', 'w', newline='') as ff:
-    print(content)
+with open('napoleon.csv', 'w', newline='',encoding='utf-8') as ff:
     for i in content:
         ff.write(f'"{i}' + '"\n')
