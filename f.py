114758ac8c387c809fed87dbc593d97bf8eb10a1
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-01-28 02:02:42 +0200

    d

--- f.py
+++ f.py
@@ -1,10 +1,13 @@
-with open('all_data.txt', 'r', encoding='utf-8') as f:
+
+with open('data.txt', 'r', encoding='utf-8') as f:
     content = f.read()
 
+content = content.replace(':\n', ': ')
 content = content.replace('“', '"')
 content = content.replace('”', '"')
 content = content.replace('’', "'")
-content = content.replace('—', '')
+content = content.replace('—', ' ')
+content = content.replace('–', ' ')
 content = content.replace('."', '".')
 content = content.replace(',"', '",')
 content = content.replace(".'", "'.")
@@ -12,9 +15,14 @@ content = content.replace(",'", "',")
 content = content.replace('  ', ' ')
 content = content.replace('\n\n', '\n')
 content = content.replace('. ', '.')
-#content = content.replace('.', '.\n')
+content = content.replace('.', '.\n')
+content = content.replace('.\n\n', '.\n')
 
 content = content.lower()
 
-with open('all_data.txt', 'w', encoding='utf-8') as f:
+for i in range(len(content)):
+    if content[i] == '"' and content[i - 1] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        content = content[:i] + ' '
+
+with open('data.txt', 'w', encoding='utf-8') as f:
     f.write(content)
--- t2csv.py
+++ t2csv.py
@@ -1,7 +1,8 @@
-
-new_content = new_content.split('\n')
-new_content = list(filter(lambda x: x != '', new_content))
-with open('Napoleon3.csv', 'w', newline='') as ff:
-    print(new_content)
-    for i in new_content:
+f = open('data.txt', 'r', encoding='utf-8')
+content = f.read()
+content = content.split('\n')
+content = list(filter(lambda x: x != '', content))
+with open('napoleon.csv', 'w', newline='') as ff:
+    print(content)
+    for i in content:
         ff.write(f'"{i}' + '"\n')
